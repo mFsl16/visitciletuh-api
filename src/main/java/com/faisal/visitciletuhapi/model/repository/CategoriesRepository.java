c239b50db3a2bdbc753ca73609352d4d7f98d6d8
mFsl16 <cd.muhammadfaisal@gmail.com>
2021-09-27 11:12:53 +0700

    update 27 sep 2021

---
+++ src/main/java/com/faisal/visitciletuhapi/model/repository/CategoriesRepository.java
@@ -0,0 +1,9 @@
+package com.faisal.visitciletuhapi.model.repository;
+
+import com.faisal.visitciletuhapi.model.entities.Categories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoriesRepository extends CrudRepository<Categories, String> {
+    
+}
